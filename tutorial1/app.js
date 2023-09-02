78bae0a2638bbef879a219577fe08708ed2dfcb4
Kiran <smkiran.09@gmail.com>
2023-09-02 14:40:11 +0530

    commit

--- tutorial1/app.js
+++ tutorial1/app.js
@@ -1,3 +1,9 @@
-console.log("hello");
-const amount = 12
-console.log();
+console.log(__dirname,"hello",__filename);
+
+
+setTimeout(()=>{
+    console.log('Kiran');
+    setInterval(()=>{
+        console.log('Hi i');
+    },1000)
+},5000) 
